d29d182d86807e33cedcb8e7bd13e27181b99f69
RedokLeeroy <Lys1999@ukr.net>
2022-07-26 13:31:07 +0300

    .

--- src/js/API/fetchdata.js
+++
@@ -1,8 +0,0 @@
-const baseURL = "http://api.weatherapi.com/v1/current.json"
-const key = "ebf0482e0efe4310b7f140703222706"
-const lang = "uk"
-
-export default function searchApi (city){
- const data = fetch(`${baseURL}?key=${key}&q=${city}&lang=${lang}`).then(response => response.json())
- return data;
-} 
